be1f815e5062f72f164bbcf09ca14608247870be
Utilisateur <frank.owen.brunet@gmail.com>
2020-08-24 20:27:11 +0200

    step 3

---
+++ hello-world/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+  
+    <MyJumbotron/>
+    
+    
+  </div>
+</template>
+
+
+<script>
+import MyJumbotron from './components/MyJumbotron.vue'
+
+//  - Then you will pass it to the MyJumbotron component as a Props.
+
+// Fourth Step : Display List
+// - The MyJumbotron component will call a ListTodo component and give to it the
+// List he received (as Props as well).
+
+// - The ListTodo component will iterate through the Array he received and will call
+// // SingleTodo component for each item of the List to looks like the picture below
+
+export default {
+  name: 'App',
+  components: {
+    MyJumbotron,
+    
+
+    
+  },
+  data () {
+      return {
+      list: [
+        {id: 0, name: "Ecrire le sujet", todo: true},
+        {id: 1, name: "Faire le sujet", todo: true},
+        {id: 2, name: "Vendre le sujet", todo: true},
+        {id: 3, name: "Partir en vaccances", todo: true}
+
+      ]
+      }
+
+    
+    
+    
+  }
+  
+  
+}
+
+
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/MyJumbotron.vue
@@ -0,0 +1,85 @@
+<template>
+<div class="jumbotron">
+  <h1 class="display-4">Todo List</h1>
+  <p class="lead">New Features  we will have to done for this project</p>
+  <hr class="my-4">
+  <p>Easy to use, we created this web app just for you</p>
+  <p class="lead">
+    
+    
+  </p>
+</div>
+</template>
+
+<script>
+import list from './App.vue'
+export default {
+  name: 'MyJumbotron',
+  
+
+}
+// new Vue({
+//   el: 'Test',
+//   data: {
+//     posts: [
+//       { id: 1, title: 'Todo List' },
+//       { id: 2, title: 'New Features we will have to done for this project'},
+//       { id: 3, title: 'Easy to use, we created this web app just for you !'}
+//     ]
+//   }
+// })
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+<script>
+
+
+// export default {
+//   name: 'Test',
+//   data() {
+//      posts: [
+//       { id: 1, title: 'Todo List' },
+//       { id: 2, title: 'New Features we will have to done for this project'},
+//       { id: 3, title: 'Easy to use, we created this web app just for you !'}
+//     ]
+     
+//       }
+// }
+
+// new Vue({
+//   el: 'Test',
+//   data: {
+//     posts: [
+//       { id: 1, title: 'Todo List' },
+//       { id: 2, title: 'New Features we will have to done for this project'},
+//       { id: 3, title: 'Easy to use, we created this web app just for you !'}
+//     ]
+//   }
+// })
+</script>
